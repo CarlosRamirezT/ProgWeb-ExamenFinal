bc1ad13f4fe81855124b7f06d2761054645fd5c5
Carlos Ramirez <38014393+CarlosRamirez@users.noreply.github.com>
2019-12-10 17:45:52 -0400

    Development 1

---
+++ ProgramacionWeb-ExamenFinal/ProgramacionWeb-ExamenFinal/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProgramacionWeb_ExamenFinal.Models;
+using System.Data.SqlClient;
+
+namespace ProgramacionWeb_ExamenFinal.Controllers
+{
+    public class UserController : Controller
+    {
+
+        SqlConnection connection = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dataReader;
+
+        [HttpGet]
+        public ActionResult Signup(int id = 0)
+        {
+            user user = new user();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Signup(user user)
+        {
+            using(UserEntities userEntiti = new UserEntities()){
+                try{
+                    if(userEntiti.users.Any(x => x.email == user.email))
+                    {
+                        ViewBag.ErrorMessage = "Ya existe un usuario con ese Email";
+                        return View("SignUp", new user());
+                    }
+                    else
+                    {
+                        userEntiti.users.Add(user);
+                        userEntiti.SaveChanges();
+                        ModelState.Clear();
+                        ViewBag.SuccessMessage = "Se ha creado el usuario: " + user.email;
+                        return View("Login");
+                    }
+                }catch(Exception exception)
+                {
+                    ViewBag.ErrorMessage = exception.StackTrace;
+                    return View("SignUp",new user());
+                }
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(user user)
+        {
+            connectionString();
+            connection.Open();
+            command.Connection = connection;
+            command.CommandText = "select * from users where email = '" + user.email + "' and password = '" + user.password + "'";
+            dataReader = command.ExecuteReader();
+            if (dataReader.Read())
+            {
+                connection.Close();
+                return RedirectToAction("Index", "Contacts");
+            }
+            else {
+                connection.Close();
+                ViewBag.ErrorMessage = "Usuario no encontrado";
+                return View("Login");
+            } 
+        }
+
+        void connectionString()
+        {
+            connection.ConnectionString = "data source=LAPTOP-2SVINSSL; database=web_final; integrated security = SSPI;";
+        }
+
+        [HttpGet]
+        public ActionResult ContactsIndex()
+        {
+            return RedirectToAction("Index", "Contacts");
+        }
+
+        [HttpGet]
+        public ActionResult VisitsIndex()
+        {
+            return RedirectToAction("Index", "Visits");
+        }
+
+    }
+}
---
+++ ProgramacionWeb-ExamenFinal/ProgramacionWeb-ExamenFinal/Controllers/VisitsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProgramacionWeb_ExamenFinal.Models;
+
+namespace ProgramacionWeb_ExamenFinal.Controllers
+{
+    public class VisitsController : Controller
+    {
+        private VisitEntities db = new VisitEntities();
+
+        // GET: Visits
+        public ActionResult Index()
+        {
+            var visits = db.Visits.Include(v => v.Contact);
+            return View(visits.ToList());
+        }
+
+        // GET: Visits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visit visit = db.Visits.Find(id);
+            if (visit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visit);
+        }
+
+        // GET: Visits/Create
+        public ActionResult Create()
+        {
+            ViewBag.idContact = new SelectList(db.Contacts, "idContact", "name");
+            return View();
+        }
+
+        // POST: Visits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "idVisit,date,entranceTime,departureTime,fullname,idContact")] Visit visit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Visits.Add(visit);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idContact = new SelectList(db.Contacts, "idContact", "name", visit.idContact);
+            return View(visit);
+        }
+
+        // GET: Visits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visit visit = db.Visits.Find(id);
+            if (visit == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idContact = new SelectList(db.Contacts, "idContact", "name", visit.idContact);
+            return View(visit);
+        }
+
+        // POST: Visits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "idVisit,date,entranceTime,departureTime,fullname,idContact")] Visit visit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(visit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idContact = new SelectList(db.Contacts, "idContact", "name", visit.idContact);
+            return View(visit);
+        }
+
+        // GET: Visits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Visit visit = db.Visits.Find(id);
+            if (visit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(visit);
+        }
+
+        // POST: Visits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Visit visit = db.Visits.Find(id);
+            db.Visits.Remove(visit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
